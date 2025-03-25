6444d2505f528a30677d8861aa9cea583194ec4b
Frank Lyaruu <flyaruu@gmail.com>
2025-03-25 13:11:45 +0100

    lint

--- src/integration_tests/mod.rs
+++ src/integration_tests/mod.rs
@@ -11,7 +11,9 @@ mod tests {
         let c = Cursor::new(DATA);
 
         let b = BufReader::new(c);
-        let statistics = run_simulation_reader(0, 100, b).await.expect("error sending message");
+        let statistics = run_simulation_reader(0, 100, b)
+            .await
+            .expect("error sending message");
         assert_eq!(10, statistics.friendlies_detected);
         assert_eq!(10, statistics.hostile_detected);
         assert_eq!(20, statistics.scans);
